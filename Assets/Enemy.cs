6c86cc6717fd9892c61dc6746362fbc4d7e06af2
Darkin <darkinb255@gmail.com>
2019-06-21 15:40:54 +0200

    Enemy Class

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int MaxHealth = 4;
+    int currHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = MaxHealth; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currHealth==0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        currHealth--;
+        currHealth = Mathf.Clamp(currHealth, 0, MaxHealth);
+        
+    }
+}
